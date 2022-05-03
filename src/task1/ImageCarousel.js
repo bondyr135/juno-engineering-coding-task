71dca0a9a44c90918c4b2da4bdf1cb1e56f417a1
Ravid Bondy <ravid135@gmail.com>
2022-05-03 11:10:53 +0300

    Did my best...

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { fetchImageUrls } from "../api/index";
 
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { CircularProgress } from '@mui/material';
+import { QrCodeScannerOutlined } from "@mui/icons-material";
+
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+
+    const [images, setImages] = useState([]);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        let images = [];
+        const getImages = async () => {
+            let promisedImages = await fetchImageUrls();
+            images = await Promise.all(promisedImages);
+            setImages(images);
+        }
+        getImages();
+    }, [])
+
+    if (!images || images.length <= 0) {
+        return <CircularProgress />
+    }
+    console.log(images)
+    return (
+        <div className="stage">
+            <ArrowBackIosNewIcon className="arrow prev" />
+            {images.map((url, idx) => {
+                < img src={url} key={idx} className={idx === index ? "slide active" : "slide"
+                } />
+            })}
+            <ArrowForwardIosIcon className="arrow next" />
+        </div>
+    )
+
 };
 export default ImageCarousel;
--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -3,27 +3,68 @@ import { allIds, fetchOrderById } from "../api";
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
-const fetchAllOrders = () => {
+const fetchAllOrders = async () => {
     const ids = allIds;
     // .....
     //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+    let allOrdersPromises = allIds.map(id => {
+        fetchOrderById(id);
+    });
+    let allOrders = await Promise.all(allOrdersPromises);
+    return allOrders;
 };
 
-const bucketOrdersByUsers = () => {
+const bucketOrdersByUsers = async () => {
     let ordersByUsers = {};
     //   2. TODO: using the function from section 1 you should now bucket the orders by user.
     // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+    let orders = await fetchAllOrders();
+    let userId;
+    orders.forEach(order => {
+        userId = order.userId;
+        if (ordersByUsers.hasOwnProperty(userId)) {
+            ordersByUsers.userId.push(order);
+        } else {
+            ordersByUsers.userId = [order];
+        }
+    });
     return ordersByUsers;
 };
 
-const getLast2WeeksOrders = () => {
+const getLast2WeeksOrders = async () => {
     //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+    let now = new Date();
+    let parseNow = now.now()
+    let twoWeeksAgo = new Date(now - 12096e5);
+    let allOrders = await fetchAllOrders();
+    let orderDate;
+    return allOrders.filter(o => {
+        orderDate = o.timestamp;
+        return (parseNow - orderDate > 12096e5 ||
+            (twoWeeksAgo.getDate() === now.getDate() && twoWeeksAgo.getMonth() === now.getMonth()))
+    });
+
 };
 
-const bucketOrdersByDate = () => {
+const bucketOrdersByDate = async () => {
     let ordersByDate = {};
     //   4. TODO: using the function from section 3 bucket the orders by date.
     // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+    let day, month, year, orderDate, dateKey;
+    let twoWeeksOrders = await getLast2WeeksOrders();
+    twoWeeksOrders.forEach(order => {
+        orderDate = new Date(order.timestamp);
+        day = orderDate.getDate();
+        day = orderDate.getMonth();
+        day = orderDate.getFullYear();
+        dateKey = `${month}-${day}-${year}`;
+
+        if (ordersByDate.hasOwnProperty(dateKey)) {
+            ordersByDate[dateKey].push(order);
+        } else {
+            ordersByDate[dateKey] = [order];
+        }
+    })
     return ordersByDate;
 };
 
